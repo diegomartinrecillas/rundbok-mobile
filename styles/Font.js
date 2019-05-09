1e510a05cae9c0a2cf60d4299213f0f3220f7253
jsirera <judsirera@gmail.com>
2019-05-09 10:44:52 +0200

    styles + bookItem

---
+++ styles/Font.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const fontStyles = StyleSheet.create({
+  big: {
+    fontSize: 20
+  },
+  small: {
+    fontSize: 16
+  },
+  boldText: {
+    fontWeight: "bold"
+  }
+});
+
+export default fontStyles;
